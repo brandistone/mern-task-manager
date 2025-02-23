d5f5f0264947544fe31e20f2f58a8c8c53cf2db1
Brandistone Mabeya <165170666+brandistone@users.noreply.github.com>
2025-02-23 23:22:06 +0300

    Update taskTypes.ts

--- frontend/mern-taskmanager/src/types/taskTypes.ts
+++ frontend/mern-taskmanager/src/types/taskTypes.ts
@@ -1,21 +1,21 @@
-// src/types/taskTypes.ts
+// // src/types/taskTypes.ts
 
-// Interface for a single task
-export interface Task {
-    _id: string;
-    title: string;
-    description: string;
-    status: 'pending' | 'in progress' | 'completed';
-    dueDate: string; // or Date if you're using Date objects
-  }
+// // Interface for a single task
+// export interface Task {
+//     _id: string;
+//     title: string;
+//     description: string;
+//     status: 'pending' | 'in progress' | 'completed';
+//     dueDate: string; // or Date if you're using Date objects
+//   }
   
-  // Interface for the props of TaskItem component
-  export interface TaskItemProps {
-    task: Task;
-  }
+//   // Interface for the props of TaskItem component
+//   export interface TaskItemProps {
+//     task: Task;
+//   }
   
-  // Interface for the props of TaskForm component
-  export interface TaskFormProps {
-    task?: Task; // Optional task for edit mode
-    onSubmit: (task: Task) => void;
-  }
+//   // Interface for the props of TaskForm component
+//   export interface TaskFormProps {
+//     task?: Task; // Optional task for edit mode
+//     onSubmit: (task: Task) => void;
+//   }
